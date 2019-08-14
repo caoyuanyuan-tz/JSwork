8bc7d1cf252b9e8aa1aef347f31e59a3ac2e67af
caoyuanyuan-tz <853681349@qq.com>
2019-08-14 19:10:15 +0800

    my project

---
+++ my-project/src/components/Register.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <form>
+    		名字<input type="text" name="name" v-model = "name"/>
+    		<br>
+    		邮箱<input type="text" name="email" v-model = "email"/>
+    		<br>
+    		密码<input type="password" name="password" v-model = "password"/>
+    		<br>
+    	</form>	
+    	<button id = "submit" @click="loadStudents">注册</button>
+    </div>
+</template>
+
+<script>
+    data():{
+        return{
+            name:'',
+            email:'',
+            password:''
+        }
+    },
+    methods:{
+        showStu(){
+            console.log(this.name,this.email,this.password)
+        }
+    }
+</script>
---
+++ my-project/src/components/Students.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <table>
+    		<tr v-for="stu in students">
+    			<td>{{stu.id}}</td>
+    			<td>{{stu.name}}</td>
+    		</tr>
+    	</table>
+    </div>
+</template>
+
+<script>
+    export default{
+        props:{
+            students:{
+                type:Array,
+                default:{{id:1,name:'xxx'}}
+            }
+        },
+        methods:{
+            showStu(stu){
+                console.log('students',stu)
+            }
+        }
+    }
+</script>
